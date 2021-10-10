fe2380a50b174b06eea2992d5351c84ae759b0c0
BenasDani <benasdnl@live.com>
2021-10-10 21:57:38 +0100

    Lab Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          
+          //creates navbar
+          <Navbar bg="primary" variant="dark">
+
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link> //links to various pages
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+
+          </Navbar>
+
+          <br />
+
+
+             //matches paths
+          <Switch>
+            <Route path='/' component={content} exact />
+            <Route path='/create' component={Header} exact />
+            <Route path='/read' component={Footer} exact />
+          </Switch>
+
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Header extends React.Component{
+
+render(){
+    return(
+        <div>
+            <h1>This is the he2ader component</h1>
+        </div>
+    )
+}
+
+
+}
